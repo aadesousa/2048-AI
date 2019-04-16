cbf9d3f1b076be1925e0e4bb77bbc3cc2e1d64b0
Adam Desousa <39541087+aadesousa@users.noreply.github.com>
2019-04-15 21:39:50 -0400

    yert
    e

---
+++ AI.py
@@ -0,0 +1,6 @@
+import string
+import random
+from fuzzywuzzy import fuzz
+import AIconstants as con
+#def art(mat):
+    
---
+++ AIconstants.py
@@ -0,0 +1,4 @@
+in_str= None
+in_str_len = None
+population = 100
+gen = 1000
